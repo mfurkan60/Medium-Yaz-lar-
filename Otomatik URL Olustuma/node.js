dfebd59252d9479def5b6087034cb0e2714ef960
Mert ERGÜDEN <mfurkanergdn@hotmail.com>
2022-01-22 15:26:18 +0300

    added Url olusturma

---
+++ Otomatik URL Olustuma/node.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    slugify = function (text) {
+        var trMap = {
+            'çÇ': 'c',
+            'ğĞ': 'g',
+            'şŞ': 's',
+            'üÜ': 'u',
+            'ıİ': 'i',
+            'öÖ': 'o'
+        };
+        for (var key in trMap) {
+            text = text.replace(new RegExp('[' + key + ']', 'g'), trMap[key]);
+        }
+        return text.replace(/[^-a-zA-Z0-9\s]+/ig, '') // remove non-alphanumeric chars
+            .replace(/\s/gi, "-") // convert spaces to dashes
+            .replace(/[-]+/gi, "-") // trim repeated dashes
+            .toLowerCase();
+
+    }
+    // Elementlere Erişme
+    $('.urlAdress').keyup(function () {
+        const value = $(this).val();
+        $(this).closest('.row').find('.slugify').val(slugify(value));
+    })
+
+
+});
